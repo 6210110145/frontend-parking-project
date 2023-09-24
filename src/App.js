3f05dedbf58a85cf0ac8b24a2d53ba0de3485949
6210110145 <6210110145@psu.ac.th>
2023-09-24 20:48:34 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/Page.css'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [transactions, setTransactions] = useState([])
+
+  const fetchTransaction = () => {
+    fetch('http://localhost:3001/transactions')
+    .then((response) => response.json())
+    .then((data) => setTransactions(data))
+  }
+
+  useEffect(() => {
+    fetchTransaction()
+  }, []);
+
+  
+return (
+    <div>
+    <h1> Show data</h1>
+      {transactions.map((transaction) => {
+        return (
+          <div className='first-page' key={transaction.transaction_id}>
+            <div className='mt-5'>
+            <h2>{transaction.car_license}</h2>
+            <h2>{transaction.car_province}</h2>
+            </div>
+          </div>
+        )
+      })}
     </div>
-  );
-}
+  )
+};
+
 
 export default App;
