0f30e65cfcc9369b344920601c0bb70ebefacc77
6210110145 <6210110145@psu.ac.th>
2023-09-27 21:03:36 +0700

    navbar

--- src/components/post.js
+++
@@ -1,1 +0,0 @@
-import './styles/Page.css';
--- src/pages/CarPage.js
+++ src/pages/CarPage.js
@@ -23,28 +23,60 @@ function CarPage() {
     }, [])*/
   
   return (
-      <div className='first-page'>
-        <div className='mt-5'>
-          <input 
-            type='text'
-            placeholder='license'
-            onChange={e => setLicense(e.target.value)}
-          />
-          <button type='button' onClick={fetchPayment}>
-            Search 
-          </button>
-          <div>
-            <a href='\payment' role="button"> go to payment </a>
-          </div>
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">หน้าหลัก</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-          return(
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/"> Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="/payment"> payment </a>
+            </li>
+          </ul>
+
+          <form class="form-inline my-2 my-lg-0">
+            <input 
+              type='text'
+              placeholder='license'
+              onChange={e => setLicense(e.target.value)}
+            />
+            <button type='button' onClick={fetchPayment}>
+              Search 
+            </button>
+          </form>
+        </div>
+      </nav>
+
+    <div className='first-page'>
+      <div class='mt-5'>
+        <div className="jumbotron">
+              
+            <input 
+              type='text'
+              placeholder='license'
+              onChange={e => setLicense(e.target.value)}
+            />
+            <button type='button' onClick={fetchPayment}>
+              Search 
+            </button>
+            <div>
+              <a href='\payment' role="button"> go to payment </a>
+            </div>
+
+          </div>
             {Object.keys(payments).map((payment) => (
               <div key={payment.payment_id}>
                 <div>{payment}: {payments[payment]} </div> 
               </div>
-            ))} 
-          )        
-        </div>
+            ))}        
+      </div>
+    </div>
     </div>
   )
 }
